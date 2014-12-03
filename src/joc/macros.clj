21533faac8559eead5d69bb83c3c9309812f051c
mahinshaw <mahinshaw@gmail.com>
2014-12-02 21:19:05 -0500

    More macros.

--- src/joc/macros.clj
+++ src/joc/macros.clj
@@ -1,6 +1,8 @@
 (ns joc.macros
   "Joy of Clojure Ch. 8: Macros"
-  (:require [clojure.walk :as walk]))
+  (:require [clojure.walk :as walk])
+  (:import (java.io BufferedReader InputStreamReader)
+           (java.net URL)))
 
 (defmacro do-until [& clauses]
   "This macro is similar to 'cond', except that it does actions until false."
@@ -29,3 +31,85 @@
 (unless (even? 2) (prn 1))
 
 (macroexpand-1 '(unless (even? 2) (prn 1)))
+
+(defmacro resolution [] `x)
+
+(macroexpand '(resolution))
+
+(def 9 x)
+(let [x 109] (resolution)) ;;=> 9
+
+(defmacro awhen [expr & body]
+  `(let [~'it ~expr]
+     (if ~'it
+       (do ~@body))))
+
+(macroexpand '(awhen [1 2 3] (it 2)))
+(awhen [1 2 3] (it 2))
+
+(macroexpand '(awhen 1 (awhen 2 [it])))
+(awhen 1 (awhen 2 [it]))
+
+(defn joc-www []
+  (-> "http://www.joyofclojure.com/hello" URL.
+      .openStream
+      InputStreamReader.
+      BufferedReader.))
+
+(let [stream (joc-www)]
+  (with-open [page stream]
+    (println (.readLine page))
+    (print "The stream will now close ..."))
+  (println "But lets read from it anyway.")
+  (.readLine stream))
+
+(defmacro with-resource [binding close-fn & body]
+  `(let ~binding
+     (try
+       (do ~@body)
+       (finally
+         (~close-fn ~(binding 0))))))
+
+(let [stream (joc-www)]
+  (with-resource [page stream]
+    #(.close %)
+    (.readLine page)))
+
+;; 8.7
+(declare collect-bodies)
+(defmacro contract [name & forms]
+  (list* `fn name (collect-bodies forms)))
+
+(declare build-contract)
+(defn collect-bodies [forms]
+  (for [form (partition 3 forms)]
+    (build-contract form)))
+
+(defn build-contract [c]
+  (let [args (first c)]
+    (list
+     (into '[f] args)
+     (apply merge
+            (for [con (rest c)]
+              (cond (= (first con) 'require)
+                    (assoc {} :pre (vec (rest con)))
+                    (= (first con) 'ensure)
+                    (assoc {} :post (vec (rest con)))
+                    :else (throw (Exception.
+                                  (str "Unknown tag "
+                                       (first con)))))))
+     (list* 'f args))))
+
+(def doubler-contract
+  (contract doubler
+            [x]
+            (require
+             (pos? x))
+            (ensure
+             (= (* 2 x) %))))
+
+(def times2 (partial doubler-contract #(* 2 %)))
+(times2 9)
+
+(def times3 (partial doubler-contract #(* 3 %)))
+(times3 9)
