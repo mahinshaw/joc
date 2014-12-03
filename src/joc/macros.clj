0436395f159167e3cc56a7fa87cfbe372f6ca22c
mahinshaw <mahinshaw@gmail.com>
2014-12-03 08:11:28 -0500

    Macros I forgot to commit.

--- src/joc/macros.clj
+++ src/joc/macros.clj
@@ -29,3 +29,61 @@
 (unless (even? 2) (prn 1))
 
 (macroexpand-1 '(unless (even? 2) (prn 1)))
+
+;; 8.4 domain example
+(defmacro domain [name & body]
+  `{:tag :domain
+    :attrs {:name (str '~name)}
+    :content [~@body]})
+
+(declare handle-things)
+
+(defmacro grouping [name & body]
+  `{:tag :grouping
+    :attrs {:name (str '~name)}
+    :content [~@(handle-things body)]})
+
+(declare grok-attrs grok-props)
+
+(defn handle-things [things]
+  (for [t things]
+    {:tag :thing
+     :attrs (grok-attrs (take-while (comp not vector?) t))
+     :content (if-let [c (grok-props (drop-while (comp not vector?) t))]
+                [c]
+                [])}))
+
+(defn grok-attrs [attrs]
+  (into {:name (str (first attrs))}
+        (for [a (rest attrs)]
+          (cond
+           (list? a) [:isa (str (second a))]
+           (string? a) [:comment a]))))
+
+(defn grok-props [props]
+  (when props
+    {:tag :properties
+     :attrs nil
+     :content (apply vector (for [p props]
+                              {:tag :property
+                               :attrs {:name (str (first p))}
+                               :content nil}))}))
+
+(def d
+  (domain man-vs-monster
+          (grouping people
+                    (Human "A stock Human")
+
+                    (Man (isa Human)
+                         "A man, baby"
+                         [name]
+                         [has-beard?]))
+          (grouping monsters
+                    (Chupacabra
+                     "A fierce, yet elusive creature"
+                     [eats-goats?]))))
+
+(:tag d)
+(:tag (first (:content d)))
+
+(clojure.xml/emit d)
